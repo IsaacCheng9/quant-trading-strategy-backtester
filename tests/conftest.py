48b4b916bb18d25a29310225bd0e00d7c6bcfdea
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2024-07-20 02:51:25 +0100

    Add `mock_pairs_data()` pytest fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 @pytest.fixture
-def mock_data():
+def mock_data() -> pd.DataFrame:
     dates = pd.date_range(start="1/1/2020", end="1/31/2020")
     return pd.DataFrame(
         {
@@ -12,6 +12,18 @@ def mock_data():
             "Low": [90] * len(dates),
             "Close": [105] * len(dates),
             "Volume": [1000000] * len(dates),
+        },
+        index=dates,
+    )
+
+
+@pytest.fixture
+def mock_pairs_data() -> pd.DataFrame:
+    dates = pd.date_range(start="1/1/2020", end="1/31/2020")
+    return pd.DataFrame(
+        {
+            "Close_1": [100 + i * 0.1 for i in range(len(dates))],
+            "Close_2": [100 + i * 0.05 for i in range(len(dates))],
         },
         index=dates,
     )
