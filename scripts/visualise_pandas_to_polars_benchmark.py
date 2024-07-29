abdcf653f621b6b4dffdf23807868ce486b64818
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2024-07-30 00:50:27 +0100

    Add support for multiple benchmarking platforms in visualiser

--- scripts/visualise_pandas_to_polars_benchmark.py
+++ scripts/visualise_pandas_to_polars_benchmark.py
@@ -29,18 +29,25 @@ def load_and_process_benchmark_data(
     return times, np.mean(times), np.std(times)
 
 
-def visualise_benchmark_times(pandas_csv: str, polars_csv: str) -> None:
+def visualise_benchmark_times(
+    benchmark_platform: str, pandas_csv: str, polars_csv: str
+) -> None:
     """
     Creates an interactive bar chart comparing pandas and Polars execution
     times using Plotly.
 
     Args:
+        benchmark_platform: The platform on which the benchmark was run
         pandas_csv: Path to CSV file with pandas execution times
         polars_csv: Path to CSV file with Polars execution times
     """
     # Read and process execution times from CSV files
     pandas_times, pandas_mean, pandas_std = load_and_process_benchmark_data(pandas_csv)
     polars_times, polars_mean, polars_std = load_and_process_benchmark_data(polars_csv)
+    # Ensure that the number of runs is equal for fairness
+    assert len(pandas_times) == len(
+        polars_times
+    ), "Different number of runs for pandas and Polars benchmarks"
     num_runs = len(pandas_times)
     run_num_labels = [f"Run {i+1}" for i in range(num_runs)]
 
@@ -119,12 +126,24 @@ def visualise_benchmark_times(pandas_csv: str, polars_csv: str) -> None:
     )
 
     # Save the plot as an interactive HTML file
-    fig.write_html("benchmark_results.html")
+    fig.write_html(f"resources/{benchmark_platform}_benchmark_results.html")
     # Show the plot (if running in an environment that supports it)
     fig.show()
 
 
 if __name__ == "__main__":
-    M1_MAX_PANDAS_CSV_PATH = "resources/m1_max_pandas_pairs_trading_with_optimisers.csv"
-    M1_MAX_POLARS_CSV_PATH = "resources/m1_max_polars_pairs_trading_with_optimisers.csv"
-    visualise_benchmark_times(M1_MAX_PANDAS_CSV_PATH, M1_MAX_POLARS_CSV_PATH)
+    PANDAS_AND_POLARS_BENCHMARKS = {
+        "m1_max": [
+            "resources/m1_max_pandas_pairs_trading_with_optimisers.csv",
+            "resources/m1_max_polars_pairs_trading_with_optimisers.csv",
+        ],
+        "streamlit": [
+            "resources/streamlit_pandas_pairs_trading_with_optimisers.csv",
+            "resources/streamlit_polars_pairs_trading_with_optimisers.csv",
+        ],
+    }
+    for platform, (
+        pandas_csv_file,
+        polars_csv_file,
+    ) in PANDAS_AND_POLARS_BENCHMARKS.items():
+        visualise_benchmark_times(platform, pandas_csv_file, polars_csv_file)
