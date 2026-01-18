579a9b6bc6902104199011498f14c3779349d4ec
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2026-01-18 01:05:18 +0000

    fix: Fix SQLAlchemy deprecation warning for `declarative_base()` import

--- src/quant_trading_strategy_backtester/models.py
+++ src/quant_trading_strategy_backtester/models.py
@@ -10,7 +10,7 @@ from sqlalchemy import (
     String,
     create_engine,
 )
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
